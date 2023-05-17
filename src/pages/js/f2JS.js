b362a750d80f1168562e7e41b0a5389815779dbf
VenomJD <jean041519@gmail.com>
2023-05-17 10:47:35 -0500

    se añadio el archivo js y se ajustaron las imagenes

---
+++ src/pages/js/f2JS.js
@@ -0,0 +1,4 @@
+const images = document.querySelector(".imgs")
+const imagesollection = document.querySelectorAll(".imgs")
+const prevBTN = document.querySelector(".prev")
+const nextBTN = document.querySelector(".next")
