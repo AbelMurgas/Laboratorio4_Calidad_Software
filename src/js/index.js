6cc67e2ab832ea9af53a1403547e060727cf9a1a
AbelMurgas <abelmurgasdev@gmail.com>
2023-05-17 23:34:26 -0500

    agregar funcion que faltaba para el carusel

--- src/js/index.js
+++ src/js/index.js
@@ -13,3 +13,44 @@ footerLinks.forEach(link => {
     link.classList.add('active');
   }
 });
+
+// Element selectors
+const prevBTN = document.querySelector(".prev");
+const nextBTN = document.querySelector(".next");
+const images = document.querySelector(".images");
+const imagesCollection = images.querySelectorAll("img");
+
+indexImage = 1
+
+// constant
+
+const amountMovePerItem = 800;
+
+// Set interval for carousel movement
+const intervalControlCarrousel = setInterval(moveCarouselNext, 5000);
+
+prevBTN.addEventListener("click", () => {
+  moveCarouselPrevious()
+});
+
+nextBTN.addEventListener("click", () => {
+  moveCarouselNext()
+});
+
+function moveCarouselNext() {
+  if (indexImage === (imagesCollection.length)){
+    indexImage = 0
+  }
+  let tansformValue = -800*(indexImage);
+  indexImage ++
+  images.style.transform = `translateX(${tansformValue}px)`;
+}
+
+function moveCarouselPrevious() {
+  if (indexImage === 0){
+    indexImage = imagesCollection.length
+  }
+  let tansformValue = -800*(indexImage-1);
+  indexImage --
+  images.style.transform = `translateX(${tansformValue}px)`;
+}
