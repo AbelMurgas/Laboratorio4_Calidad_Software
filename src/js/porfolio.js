b7c662154cc7e3805f1109f6ec7e9b94f76ed33b
AbelMurgas <abelmurgasdev@gmail.com>
2023-05-23 18:44:51 -0500

    add porfolio js

--- src/js/porfolio.js
+++ src/js/porfolio.js
@@ -1,19 +1,3 @@
-// Get the current page URL
-const currentPageURL = window.location.href;
-
-// Get all the footer links
-const footerLinks = document.querySelectorAll('footer ul li a');
-
-// Loop through the footer links
-footerLinks.forEach(link => {
-  // Check if the link href matches the current page URL
-  console.log(link.href, currentPageURL)
-  if (link.href === currentPageURL) {
-    // Add a class to the active link
-    link.classList.add('active');
-  }
-});
-
 var isFirstClick = true;
 
 function togglePortfolioOptions() {
